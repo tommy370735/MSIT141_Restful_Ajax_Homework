d0867c90c65f878b0307b9f79a191813c672f4a2
tommy370735 <tommy370735@gmail.com>
2022-07-01 19:19:45 +0800

    加入專案檔案。

---
+++ MSIT141_Restful_Ajax_Homework/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MSIT141_Restful_Ajax_Homework.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
